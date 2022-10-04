04af002e035800bcf2fd3780e0cf4c84222a720e
ncavchl <ncavchl@ajou.ac.kr>
2022-10-04 21:57:33 +0900

    💄 컴포넌트 스타일

--- src/app/todo/todos/add-todo/add-todo.component.ts
+++ src/app/todo/todos/add-todo/add-todo.component.ts
@@ -3,11 +3,39 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-add-todo',
   template: `
+    <button (click)="addTodo(newText)">+</button>
     <input type="text" placeholder="할 일 추가" [(ngModel)]="newText"/>
-    <button (click)="addTodo(newText)">Add</button>
+   
   `,
-  styles: [
-  ]
+  styles: [`
+    :host {
+      display:block;
+      padding:16px;
+      background-color:white;
+    }
+    input {
+      display:inline-block;
+      font-size:18px;
+      border:none;
+    }
+    input:focus {
+      border-bottom:1px solid darkblue;
+    }
+    input:focus, button:focus {
+      outline:none;
+    }
+    button {
+      width:24px;
+      height:24px;
+      border-radius:24px;
+      color:white;
+      font-size:16px;
+      line-height:17px;
+      border: 1px solid dimgray;
+      background-color: darkblue;
+      margin-right:10px;
+    }
+  `]
 })
 export class AddTodoComponent implements OnInit {
   @Output() onTodoAdded = new EventEmitter();
--- src/app/todo/todos/todo/todo.component.ts
+++ src/app/todo/todos/todo/todo.component.ts
@@ -4,9 +4,53 @@ import { Todo } from '../../share/todo.model';
 @Component({
   selector: 'app-todo',
   template: `
-    <input type="checkbox" [checked]="todo?.done">{{todo?.text}}
+    <input type="checkbox" [checked]="todo?.done"><label>{{todo?.text}}</label>
   `,
   styles: [
+    `
+    :host {
+      display:block;
+      padding:16px;
+      color:darkgray;
+      background-color:white;  
+    }
+    input {
+      position:relative;
+    }
+    input:before {
+      content:'';
+      display:inline-block;
+      width:18px;
+      height:18px;
+      background-color:white;
+      border-radius:18px;
+      position:absolute;
+      top:-2px;
+      left:-4px;
+      border:1px solid dimgray;
+    }
+    input:checked:after {
+      content:'\\2713';
+      font-size:14px;
+      display:inline-block;
+      width:18px;
+      height:18px;
+      border-radius:18px;
+      position:absolute;
+      top:-2px;
+      left:-4px;
+      border:1px solid darkgray;
+      background-color:dimgray;
+      text-align:center;
+      color:white;
+    }
+    input + label {
+      margin-left:5px;
+    }
+    input:checked + label {
+      text-decoration:line-through;
+    }
+    `
   ]
 })
 export class TodoComponent implements OnInit {
