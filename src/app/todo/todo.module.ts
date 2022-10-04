230fc378b972ced5b78f5fcb168329241ae7ddfb
ncavchl <ncavchl@ajou.ac.kr>
2022-10-04 20:13:35 +0900

    🎨 todo 세팅

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodosComponent } from './todos/todos.component';
+import { TodoComponent } from './todo/todo.component';
 
 
 
 @NgModule({
   declarations: [
-    TodosComponent
+    TodosComponent,
+    TodoComponent
   ],
   imports: [
     CommonModule
--- src/app/todo/todos/todos.component.ts
+++ src/app/todo/todos/todos.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// @Component - 데코레이터 / 함수처럼 호출 / 메타데이터를 보냄 (속성) / 이 컴포넌트가 어떻게 동작하는지 정의
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  selector: 'app-todos', //선택자
+  templateUrl: './todos.component.html', //뷰에 대한 정의
+  styleUrls: ['./todos.component.css'] // 스타일 , 여러개 가질 수 있음
 })
 export class TodosComponent implements OnInit {
 
@@ -13,3 +14,6 @@ export class TodosComponent implements OnInit {
   }
 
 }
+
+// class에서 속성과 메소드를 통해 view와 데이터를 주고받음 
+
